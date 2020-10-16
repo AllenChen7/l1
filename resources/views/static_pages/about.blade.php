e983838db88acc7950a49dafed3d29c234ba7723
chen <1175195335@qq.com>
2020-10-16 16:29:56 +0800

    DRY
    don't repeat youself

--- resources/views/static_pages/about.blade.php
+++ resources/views/static_pages/about.blade.php
@@ -1,10 +1,5 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>L1</title>
-    </head>
-
-    <body>
-        <h1>关于</h1>
-    </body>
-</html>
+@extends('layouts.default')
+@section('title', '关于')
+@section('content')
+    <h1>关于</h1>
+@stop
--- resources/views/static_pages/help.blade.php
+++ resources/views/static_pages/help.blade.php
@@ -1,9 +1,5 @@
-<html>
-    <head>
-        <title>L1</title>
-    </head>
-
-    <body>
-        <h1>帮助</h1>
-    </body>
-</html>
+@extends('layouts.default')
+@section('title', '帮助')
+@section('content')
+    <h1>帮助</h1>
+@stop
