eb3aef41f3f6aa9085260f94767182af4d4ffb5d
chen <1175195335@qq.com>
2020-10-16 17:03:42 +0800

    npm
    yarn

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,18 @@
 <html>
     <head>
         <title>@yield('title', 'L1')</title>
+        <link rel="stylesheet" href="/css/app.css">
     </head>
 
     <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="/">L1</a>
+                <ul class="navbar-nav justify-content-end">
+                    <li></li>
+                </ul>
+            </div>
+        </nav>
         @yield('content')
     </body>
 </html>
