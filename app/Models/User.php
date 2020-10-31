835184cb4831ca8c2029505196ce6061e3411a9e
chen <chen@test.com>
2020-11-01 00:33:09 +0800

    email
    email

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
@@ -46,5 +47,14 @@ class User extends Authenticatable
     public function gravatarName()
     {
         return 'https://ui-avatars.com/api/?background=random&name=' . $this->attributes['name'];
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($user) {
+            $user->activation_token = Str::random(10);
+        });
     }
 }
